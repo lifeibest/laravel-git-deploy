f8a7b74cd76cce4e38a74716b5b7e3b6aba7fa64
李飞 <123178784@qq.com>
2018-02-28 14:36:55 +0800

    src

---
+++ src/GitDeployServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+namespace Lifeibest\GitDeploy;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class GitDeployServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->registerRoutes();
+        $this->registerResources();
+        $this->definePublishing();
+    }
+    /**
+     * Register the Redis manager routes.
+     *
+     * @return void
+     */
+    protected function registerRoutes()
+    {
+
+    }
+    /**
+     * Register the Redis manager resources.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'git-deploy');
+    }
+    /**
+     * Define the publishing.
+     *
+     * @return void
+     */
+    public function definePublishing()
+    {
+
+    }
+}
